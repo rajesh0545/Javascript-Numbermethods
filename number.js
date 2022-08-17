055a1bbb310194f58234e5a56b617492c389b811
rajesh <krajeshgowri7032@mail.com>
2022-08-17 16:09:47 +0530

    Javascript-Number

---
+++ number.js
@@ -0,0 +1,15 @@
+// number methods
+// var x=2345;
+// var y=2345.785;
+
+// console.log(Number.isFinite(x));
+// console.log(Number.isInteger(x));
+// console.log(Number.isNaN(x));
+// console.log(Number.isInteger(y));
+// console.log(Number.parseFloat(y));
+// console.log(Number.parseInt(x));
+// console.log(Number.tofixed(x));
+
+//number
+let x=Number.MAX_VALUE;
+console.log(x);
